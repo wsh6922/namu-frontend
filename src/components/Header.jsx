72b67d2d96b85345cc66bffc355b9dec6eae6fdc
Mi1esian <wsh6922@naver.com>
2024-10-09 00:26:15 +0900

    헤더 60% 작업

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,38 @@
 // import React from "react";
+import { useState } from "react";
 
 const Header = () => {
 
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
     return (
     <div id="top_navbar" className="navbar flexd-top" style={{transform: 'translateY(0px)'}}>
-       <div className="top-navbar-content">
-            <div className="nav-button-bar">
+        <div className="top-navbar-content">
+            <div className="nav-button-left-bar">
                 <button id="dark-mode" className="nav-button">
                     <i className="icon-brightness-contrast"></i>
                 </button>
-                <div>
-                    <input></input>
-                </div>
-                <button>
-                    
-                </button>
             </div>
-       </div>
+            <div className="search-bar">
+                <input id="searchbar-input" type="text" placeholder="입력하세요"></input>
+            </div>
+            <div className="nav-button-right-bar">
+                { isLoggedIn ? (
+                    <>
+                        <button type="button" className="nav-button-items">
+                            로그아웃
+                        </button>
+                        <button type="button" className="nav-button-items">
+                            글쓰기
+                        </button>
+                    </>
+                ) : (
+                        <button type="button" className="nav-button-items">
+                            로그인
+                        </button>
+                )}
+            </div>
+        </div>
     </div>
     )
 };
