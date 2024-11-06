3e57d59904c1f41004903ff8d41e0e1771cd78f0
summer <wsh6922@naver.com>
2024-11-06 10:00:42 +0900

    About 완료

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -35,7 +35,7 @@ const Development = () => {
                                 배움에서 그치지 않고 학습한 지식을 <strong>Github</strong>와 <strong>기술 블로그</strong>를<br />
                                 통해 체계적으로 <strong>기록</strong>하고 <strong>공유</strong>하고 있습니다.<br />
                                 꾸준한 기록의 <strong>가치</strong>를 믿으며, 한 단계씩 성장하고자 합니다.<br />
-                                저에 대한 자세한 내용은 <strong>이력서</strong>와 <strong>포트폴리오</strong>에서 확인하실 수 있습니다!
+                                저에 대한 자세한 내용은 <strong><a href="">이력서</a></strong>와 <strong><a href="">포트폴리오</a></strong>에서 확인하실 수 있습니다!
                             </p>
                             <div className="sidebar-social">
                                 <ul>
